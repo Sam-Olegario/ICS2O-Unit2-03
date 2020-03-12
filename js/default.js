9c9dbb2262452666d7743c470527594542970b54
Ubuntu <ubuntu@ip-172-31-84-94.ec2.internal>
2020-03-12 16:58:30 +0000

    fuck4

--- js/default.js
+++ js/default.js
@@ -1,1 +1,9 @@
 // JavaScript File
+document.getElementById("act").addEventListener("click",myFunction);
+  function myFunction() {
+    alert("1");
+    alert("2");
+    alert("3");
+    alert("4");
+    alert("5");
+  }
